c187e41faa4b5bd3cee35d5565f997a55dc20488
Jodick Ndayisenga <rajajodick@gmail.com>
2022-10-24 14:27:53 +0300

    'colors.html'

---
+++ changecolor.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('select').onchange = function() {
+        document.querySelector('#hello').style.color = this.value;
+    }
+
+
+})
